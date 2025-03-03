84f14f47bf716ae8e440d34b37ed235ffdb7b9dc
pengzhaoyang <12739792+pengzhaoyang101@user.noreply.gitee.com>
2025-03-03 10:54:10 +0800

    feat:图片库 先移除ios 部分

--- ios/Classes/PowerImagePlugin.h
+++
@@ -1,6 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface PowerImagePlugin : NSObject<FlutterPlugin, FlutterStreamHandler>
-+ (instancetype)sharedInstance;
-- (void)sendImageStateEvent:(NSMutableDictionary *)event success:(BOOL)success;
-@end
